d83ed45d6d9dbcf3574e60646efbb9061b49ec22
iLoveDataJjia <ilovedata.jjia@gmail.com>
2023-05-13 21:16:48 +0200

    - Init git - WIP on design build section

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BuildPg } from "./pages/build/BuildPg";
+
+/**
+ * Entrypoint.
+ */
+export default function App() {
+  return (
+    <div className="text-white">
+      <BuildPg />
+    </div>
+  );
+}
