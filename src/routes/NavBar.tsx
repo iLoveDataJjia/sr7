38a1163830c890ed542b1ae0f9fc2cd151b4b5bc
iLoveDataJjia <ilovedata.jjia@gmail.com>
2023-06-13 07:28:02 +0200

    DONE: Fix search bar

--- src/routes/NavBar.tsx
+++ src/routes/NavBar.tsx
@@ -1,8 +1,45 @@
 import { ReactComponent as Search } from "../assets/general/search.svg";
 import { ReactComponent as Star } from "../assets/general/star.svg";
-import { getCharacters, displayable, tierCSS } from "../data/Characters";
-import { useEffect, useState } from "react";
+import { getCharacters, displayable, tierCSS, Character } from "../data/Characters";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+
+/**
+ * Search bar result.
+ */
+function SearchBarResult({ char, idx, className }: { char: Character; idx: number; className?: string }) {
+  // CSS
+  const bgCss = idx % 2 === 0 ? "bg-indigo-900 hover:bg-indigo-700" : "bg-indigo-950 hover:bg-indigo-800";
+  const tierColorCSS = tierCSS(char.dynamic.tier);
+
+  // Render
+  return (
+    <Link
+      to={`/characters/${char.nameUID.toLowerCase()}`}
+      key={char.nameUID}
+      className={`flex items-center px-4 py-2 ${bgCss}` + (className ? ` ${className}` : "")}
+      onClick={() => window.scroll({ top: 0, left: 0 })}
+    >
+      <img src={`/characters/${char.nameUID}/photo.webp`} alt={char.nameUID} className="h-10 w-10 rounded-md" />
+      <p className="px-8 text-2xl font-bold">{displayable(char.nameUID)}</p>
+      <div className="ml-auto flex items-center space-x-3">
+        <div className="flex flex-col items-center space-y-1">
+          <div className="flex items-center space-x-0.5">
+            <p className="font-bold">{char.static.star}</p>
+            <Star className="h-4 w-4 fill-amber-500" />
+          </div>
+          <div
+            className={`rounded-md border-[1px] ${tierColorCSS.border} w-7 text-center text-xs font-bold ${tierColorCSS.text}`}
+          >
+            {char.dynamic.tier}
+          </div>
+        </div>
+        <img src={`/elements/${char.static.element}.webp`} alt={char.static.element} className="h-7 w-7" />
+        <img src={`/paths/${char.static.path}.webp`} alt={char.static.path} className="h-7 w-7" />
+      </div>
+    </Link>
+  );
+}
 
 /**
  * Search bar.
@@ -12,20 +49,6 @@ function SearchBar() {
   const [input, setInput] = useState("");
   const [isFocus, setIsFocus] = useState(false);
   const chars = getCharacters().filter((_) => _.nameUID.toLowerCase().startsWith(input.toLowerCase()));
-
-  // CSS
-  const roundedCss = (idx: number) => {
-    switch (idx) {
-      case 0:
-        return chars.length === 1 ? "rounded-md" : "rounded-t-md";
-      case chars.length - 1:
-        return "rounded-b-md";
-      default:
-        return "";
-    }
-  };
-  const bgCss = (idx: number) =>
-    idx % 2 === 0 ? "bg-indigo-900 hover:bg-indigo-700" : "bg-indigo-950 hover:bg-indigo-800";
 
   // Render
   return (
@@ -38,47 +61,28 @@ function SearchBar() {
         className="grow bg-transparent text-sm focus:outline-none"
         onChange={(e) => setInput(e.target.value)}
         onFocus={() => setIsFocus(true)}
-        onBlur={() => setTimeout(() => setIsFocus(false), 20)}
+        onBlur={() => setTimeout(() => setIsFocus(false), 100)}
       />
 
       {/* On change & focus */}
       {input !== "" && isFocus && (
         <div className="absolute -left-4 top-full flex w-max min-w-full translate-y-4 flex-col justify-center shadow">
-          {chars.map((char, idx) => {
-            // CSS
-            const tierColorCSS = tierCSS(char.dynamic.tier);
-
-            // Render
-            return (
-              <Link
-                to={`/characters/${char.nameUID.toLowerCase()}`}
-                key={char.nameUID}
-                className={`flex items-center p-4 ${bgCss(idx)} ${roundedCss(idx)}`}
-              >
-                <img
-                  src={`/characters/${char.nameUID}/photo.webp`}
-                  alt={char.nameUID}
-                  className="h-12 w-12 rounded-md"
-                />
-                <p className="px-8 text-2xl font-bold">{displayable(char.nameUID)}</p>
-                <div className="ml-auto flex items-center space-x-2.5">
-                  <div className="flex flex-col items-center space-y-0.5">
-                    <div className="flex items-center space-x-0.5">
-                      <p className="font-bold">{char.static.star}</p>
-                      <Star className="h-4 w-4 fill-amber-500" />
-                    </div>
-                    <div
-                      className={`rounded-md border-2 ${tierColorCSS.border} w-8 text-center text-sm font-bold ${tierColorCSS.text}`}
-                    >
-                      {char.dynamic.tier}
-                    </div>
-                  </div>
-                  <img src={`/elements/${char.static.element}.webp`} alt={char.static.element} className="h-7 w-7" />
-                  <img src={`/paths/${char.static.path}.webp`} alt={char.static.path} className="h-7 w-7" />
-                </div>
-              </Link>
-            );
-          })}
+          {chars.map((char, idx) => (
+            <SearchBarResult
+              key={idx}
+              char={char}
+              idx={idx}
+              className={
+                chars.length === 1
+                  ? "rounded-md"
+                  : idx === 0
+                  ? "rounded-t-md"
+                  : idx === chars.length - 1
+                  ? "rounded-b-md"
+                  : ""
+              }
+            />
+          ))}
         </div>
       )}
     </div>
@@ -99,7 +103,7 @@ export default function NavBar({ className }: { className?: string }) {
       </a>
       <SearchBar />
       <p className="w-4/12 break-all text-justify text-xs font-thin italic text-indigo-300">
-        {window.location.host.toUpperCase()} - Build hub for Honkai Impact Star Rail. Unleash the power of your beloved
+        {window.location.host.toUpperCase()} - Honkai Impact Star Rail build hub. Unleash the power of your beloved
         waifus and husbandos!
       </p>
     </div>
