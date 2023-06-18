d1e9d980d64ff036ed85df7eed0ca6f6c356880d
iLoveDataJjia <ilovedata.jjia@gmail.com>
2023-06-18 14:08:19 +0200

    Add 404 handling

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 /**
- * Handle not found. (Only for dev with static "export")
+ * Handle not found. (Only for production with static "export")
  */
 export default function NotFound() {
   const router = useRouter();
